cfefbd2dfdf1ad19fbceb690dc9906dde64bd892
omotorvidosky <omotorvidosky@gmail.com>
2023-10-14 12:48:17 +0100

    Completed Sign Up page UI

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const SignUp = () => {
   return (
-    <div>SignUp</div>
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl text-center font-semibold my-7'>SignUp</h1>
+      <form className='flex flex-col gap-4'>
+        <input type="text" placeholder='username'
+          className='border p-3 rounded-lg' id='username' />
+        <input type="email" placeholder='email'
+          className='border p-3 rounded-lg' id='email' />
+        <input type="password" placeholder='password'
+          className='border p-3 rounded-lg' id='password' />
+        <input type="password" placeholder='confirm password'
+        className='border p-3 rounded-lg' id='confirmPassword'/>
+        <button className='bg-slate-700 text-white p-3
+        rounded-lg uppercase hover:opacity-80
+        disabled:opacity-50'>Sign Up</button>
+        
+      </form>
+      <div className='flex gap-2 mt-5'>
+        <p>Have an account? </p>
+        <Link to='/sign-in'>
+          <span className='text-blue-700'>Sign in</span>
+        </Link>
+      </div>
+    </div>
   )
 }
 
