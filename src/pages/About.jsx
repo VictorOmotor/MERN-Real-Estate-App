7d8e35cb29303d8477a4e9d30583f0fd4672024d
omotorvidosky <omotorvidosky@gmail.com>
2023-10-24 15:07:26 +0100

    Completed about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,42 @@
 import React from 'react'
 
 const About = () => {
-  return <div>About</div>
+  return (
+    <div className="py-10 px-4 max-w-6xl mx-auto">
+      <h1
+        className="text-3xl font-bold mb-4
+      text-slate-800"
+      >
+        About Platinum Estate
+      </h1>
+      <p className="mb-4 text-slate-700">
+        <span className="text-[40px] font-semibold text-slate-600">P</span>
+        latinum Estate is a leading real estate agency in Nigeria that
+        specializes in helping clients buy, sell, and rent properties in the
+        most desirable neighbourhoods. Our team of experienced agents is
+        dedicated to providing exceptional services and making the buying and
+        selling process as smooth as possible.
+      </p>
+      <p className="mb-4 text-slate-700">
+        Our mission is to help our clients achieve their real estate goals by
+        providing expert advice, personlized service , and a deep understanding
+        of the local market. Whether you are looking to buy, sell, or rent a
+        property, we are here to help youevery step of the way
+      </p>
+      <p className="mb-4 text-slate-700">
+        Our team of agents possess remarkable wealth of experience and knowledge
+        in the real estate industry, and we are committed to providing the
+        highest level of service to our clients in line with statutory laws and
+        regulations guiding real estate agency and management in Nigeria.
+      </p>
+      <p className="mb-4 text-slate-700">
+        We believe that having an affordable and seamless property buying,
+        renting and selling experience should be the exclusive preserve of the
+        Nigerian populace, and we are poised to revolutionalise service delivery
+        for Nigerians in the housing industry.
+      </p>
+    </div>
+  )
 }
 
 export default About
